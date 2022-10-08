1b032d26df67d2a60492e137fa85e2d51184e6f9
rzahrohkh <170441100011@student.trunojoyo.ac.id>
2022-10-08 23:29:05 +0700

    creating navbar

---
+++ application/views/templates_landing/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title><?= $title ?></title>
+    <meta content="" name="description">
+
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="<?php base_url() ?>asset_landing/img/favicon.png" rel="icon">
+    <link href="<?php base_url() ?>asset_landing/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="<?php base_url() ?>asset_landing/vendor/aos/aos.css" rel="stylesheet">
+    <link href="<?php base_url() ?>asset_landing/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php base_url() ?>asset_landing/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="<?php base_url() ?>asset_landing/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="<?php base_url() ?>asset_landing/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="<?php base_url() ?>asset_landing/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="<?php base_url() ?>asset_landing/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
---
+++ application/views/templates_landing/navbar.php
@@ -0,0 +1,30 @@
+    <!-- ======= Header ======= -->
+    <header id="header" class="header fixed-top">
+        <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+            <a href="index.html" class="logo d-flex align-items-center">
+                <img src="<?php base_url() ?>asset_landing/img/logo.png" alt="">
+                <span><?= $title ?></span>
+            </a>
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link scrollto active" href="#beranda">Beranda</a></li>
+                    <li><a class="nav-link scrollto" href="#profil">Profil Klinik</a></li>
+                    <li><a class="nav-link scrollto" href="#pelayanan">Pelayanan</a></li>
+
+                    <li class="dropdown"><a href="#"><span>Informasi</span> <i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="#">Dokter</a></li>
+                            <li><a href="#">Hubungi Kami</a></li>
+                            <li><a href="#">Jadwal Praktek</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a class="getstarted scrollto" href="<?php base_url() ?>/login">Masuk</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+
+        </div>
+    </header><!-- End Header -->
