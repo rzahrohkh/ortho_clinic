0cb7ba85643e535272450e76e49eb777d91f18ea
rzahrohkh <170441100011@student.trunojoyo.ac.id>
2022-12-09 21:17:04 +0700

    fix bug section model

--- application/models/Section_model.php
+++ application/models/Section_model.php
@@ -31,4 +31,12 @@ class Section_model  extends CI_model // sesui dengan nama tabel di db
         $this->db->update('section_clinic', $data, ['id_section' => $id_section]);
         return $this->db->affected_rows();
     }
+
+    public function getSectionByType($typeSection)
+    {
+        $this->db->select('*');
+        $this->db->from('section_clinic');
+        $this->db->where('type_section', $typeSection);
+        return $this->db->get()->row_array();
+    }
 }
