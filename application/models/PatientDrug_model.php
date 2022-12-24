3052b4bfa9b31cb949398da5f4c2f62b6ebc6177
rzahrohkh <170441100011@student.trunojoyo.ac.id>
2022-12-24 20:15:26 +0700

    [dashboard] patient drug feature fix bug

--- application/models/PatientDrug_model.php
+++ application/models/PatientDrug_model.php
@@ -6,16 +6,16 @@ class PatientDrug_model  extends CI_model // sesui dengan nama tabel di db
         return $this->db->query("SELECT * FROM drug_patient")->result_array();
     }
 
-    public function get_drug_patient_byID($id_sesuitabeldidb)
+    public function get_drug_patient_byID($id_drugs_patient)
     {
         $this->db->select('*');
         $this->db->from('drug_patient');
-        $this->db->where('id_sesuitabeldidb', $id_sesuitabeldidb);
+        $this->db->where('id_sesuitabeldidb', $id_drugs_patient);
         return $this->db->get()->row_array();
     }
-    public function delete_drug_patient($id_sesuitabeldidb)
+    public function delete_drug_patient($id_drugs_patient)
     {
-        $this->db->where('id_sesuitabeldidb', $id_sesuitabeldidb);
+        $this->db->where('id_drugs_patient', $id_drugs_patient);
         $this->db->delete('drug_patient');
     }
 
@@ -26,9 +26,9 @@ class PatientDrug_model  extends CI_model // sesui dengan nama tabel di db
 
     public function update_drug_patient(
         $data,
-        $id_sesuitabeldidb
+        $id_drugs_patient
     ) {
-        $this->db->update('drug_patient', $data, ['id_sesuitabeldidb' => $id_sesuitabeldidb]);
+        $this->db->update('drug_patient', $data, ['id_drugs_patient' => $id_drugs_patient]);
         return $this->db->affected_rows();
     }
 }
