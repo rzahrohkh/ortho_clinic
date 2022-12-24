1fce8d04eebf7264b3d715ef037942eb33d9cd00
rzahrohkh <170441100011@student.trunojoyo.ac.id>
2022-12-24 22:01:52 +0700

    [dashboard] patient feature fix bug

--- application/views/dp/patient/index.php
+++ application/views/dp/patient/index.php
@@ -13,56 +13,62 @@
             <div class="col-12">
                 <div class="row">
 
-                    <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card">
-                            <div class="card-body px-4 py-4-5">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                                        <div class="stats-icon blue mb-2">
-                                            <i class="iconly-boldProfile"></i>
+                    <div class="col-6 col-lg-4 col-md-6">
+                        <a href="<?= base_url('ControlPatientSchedule') ?>">
+                            <div class="card" style="height: 80%;">
+                                <div class="card-body px-4 py-4-5">
+                                    <div class="row">
+                                        <div class="px-1 col-md-2 col-lg-12 col-xl-12 col-xxl-2 d-flex justify-content-start ">
+                                            <div class="stats-icon blue mb-2">
+                                                <i class="fa-solid fa-stethoscope"></i>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="text-muted font-semibold">Followers</h6>
-                                        <h6 class="font-extrabold mb-0">183.000</h6>
+                                        <div class="col-md-10 col-lg-12 col-xl-12 col-xxl-10">
+                                            <h6 class="text-muted font-semibold">Jadwal Kontrol</h6>
+                                            <h6 class="font-extrabold mb-0">Jadwal Kontrol Ke Klinik</h6>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
-                    <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card">
-                            <div class="card-body px-4 py-4-5">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                                        <div class="stats-icon green mb-2">
-                                            <i class="iconly-boldAdd-User"></i>
+                    <div class="col-6 col-lg-4 col-md-6">
+                        <a href="<?= base_url('PatientActivity') ?>">
+                            <div class="card" style="height: 80%;">
+                                <div class=" card-body px-4 py-4-5">
+                                    <div class="row">
+                                        <div class="px-1 col-md-2 col-lg-12 col-xl-12 col-xxl-2 d-flex justify-content-start ">
+                                            <div class="stats-icon green mb-2">
+                                                <i class="fa-solid fa-person-walking"></i>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="text-muted font-semibold">Following</h6>
-                                        <h6 class="font-extrabold mb-0">80.000</h6>
+                                        <div class="col-md-10 col-lg-12 col-xl-12 col-xxl-10">
+                                            <h6 class="text-muted font-semibold">Aktivitas</h6>
+                                            <h6 class="font-extrabold mb-0">Kegiatan Harian Anda</h6>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
-                    <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card">
-                            <div class="card-body px-4 py-4-5">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                                        <div class="stats-icon red mb-2">
-                                            <i class="iconly-boldBookmark"></i>
+                    <div class="col-6 col-lg-4 col-md-6">
+                        <a href="<?= base_url('PatientDrug') ?>">
+                            <div class="card" style="height: 80%;">
+                                <div class="card-body px-4 py-4-5">
+                                    <div class="row">
+                                        <div class="px-1 col-md-2 col-lg-12 col-xl-12 col-xxl-2 d-flex justify-content-start ">
+                                            <div class="stats-icon red mb-2">
+                                                <i class="fa-solid fa-capsules"></i>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="text-muted font-semibold">Saved Post</h6>
-                                        <h6 class="font-extrabold mb-0">112</h6>
+                                        <div class="col-md-10 col-lg-12 col-xl-12 col-xxl-10">
+                                            <h6 class="text-muted font-semibold">Obat Anda</h6>
+                                            <h6 class="font-extrabold mb-0">Obat Yang Di Konsumsi</h6>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                 </div>
             </div>
