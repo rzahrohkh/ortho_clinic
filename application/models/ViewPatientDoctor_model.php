d0eaf1b684d7b241acf4d1871fcacf2ac47c4d66
rzahrohkh <170441100011@student.trunojoyo.ac.id>
2022-12-24 20:29:15 +0700

    [dashboard] view patient for doctor feature

---
+++ application/models/ViewPatientDoctor_model.php
@@ -0,0 +1,34 @@
+<?php
+class ViewPatientDoctor_model  extends CI_model // sesui dengan nama tabel di db
+{
+    public function get_patient_all()
+    {
+        return $this->db->query("SELECT * FROM patient")->result_array();
+    }
+
+    public function get_patient_byID($id_patient)
+    {
+        $this->db->select('*');
+        $this->db->from('patient');
+        $this->db->where('id_patient', $id_patient);
+        return $this->db->get()->row_array();
+    }
+    public function delete_patient($id_patient)
+    {
+        $this->db->where('id_patient', $id_patient);
+        $this->db->delete('patient');
+    }
+
+    public function add_patient($data)
+    {
+        $this->db->insert('patient', $data);
+    }
+
+    public function update_patient(
+        $data,
+        $id_patient
+    ) {
+        $this->db->update('patient', $data, ['id_patient' => $id_patient]);
+        return $this->db->affected_rows();
+    }
+}
