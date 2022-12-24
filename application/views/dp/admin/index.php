c8d0e1fccaef1b61b0624aa5617f8596593cb831
rzahrohkh <170441100011@student.trunojoyo.ac.id>
2022-12-24 22:13:48 +0700

    [dashboard] admin feature fix bug

--- application/views/dp/admin/index.php
+++ application/views/dp/admin/index.php
@@ -13,16 +13,16 @@
             <div class="col-12">
                 <div class="row">
 
-                    <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card">
+                    <div class="col-6 col-lg-4 col-md-6">
+                        <div class="card" style="height: 80%;">
                             <div class="card-body px-4 py-4-5">
                                 <div class="row">
-                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                                    <div class="px-1 col-md-2 col-lg-12 col-xl-12 col-xxl-2 d-flex justify-content-start ">
                                         <div class="stats-icon blue mb-2">
                                             <i class="iconly-boldProfile"></i>
                                         </div>
                                     </div>
-                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                    <div class="col-md-10 col-lg-12 col-xl-12 col-xxl-10">
                                         <h6 class="text-muted font-semibold">Followers</h6>
                                         <h6 class="font-extrabold mb-0">183.000</h6>
                                     </div>
@@ -30,16 +30,16 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card">
+                    <div class="col-6 col-lg-4 col-md-6">
+                        <div class="card" style="height: 80%;">
                             <div class="card-body px-4 py-4-5">
                                 <div class="row">
-                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                                    <div class="px-1 col-md-2 col-lg-12 col-xl-12 col-xxl-2 d-flex justify-content-start ">
                                         <div class="stats-icon green mb-2">
                                             <i class="iconly-boldAdd-User"></i>
                                         </div>
                                     </div>
-                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                    <div class="col-md-10 col-lg-12 col-xl-12 col-xxl-10">
                                         <h6 class="text-muted font-semibold">Following</h6>
                                         <h6 class="font-extrabold mb-0">80.000</h6>
                                     </div>
@@ -47,16 +47,16 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card">
+                    <div class="col-6 col-lg-4 col-md-6">
+                        <div class="card" style="height: 80%;">
                             <div class="card-body px-4 py-4-5">
                                 <div class="row">
-                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                                    <div class="col-md-2 col-lg-12 col-xl-12 col-xxl-2 d-flex justify-content-start ">
                                         <div class="stats-icon red mb-2">
                                             <i class="iconly-boldBookmark"></i>
                                         </div>
                                     </div>
-                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                                    <div class="col-md-10 col-lg-12 col-xl-12 col-xxl-10">
                                         <h6 class="text-muted font-semibold">Saved Post</h6>
                                         <h6 class="font-extrabold mb-0">112</h6>
                                     </div>
