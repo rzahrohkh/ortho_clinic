2b746da61d7f0edbc633b06dc76d8fb6115554fb
rzahrohkh <170441100011@student.trunojoyo.ac.id>
2022-12-24 21:56:54 +0700

    [dashboard] nurse fix bug

--- application/views/dp/nurse/index.php
+++ application/views/dp/nurse/index.php
@@ -13,57 +13,65 @@
             <div class="col-12">
                 <div class="row">
 
-                    <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card">
-                            <div class="card-body px-4 py-4-5">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                                        <div class="stats-icon blue mb-2">
-                                            <i class="iconly-boldProfile"></i>
+                    <div class="col-6 col-lg-4 col-md-6">
+                        <a href="<?= base_url('ViewPatientNurse') ?>">
+                            <div class="card" style="height: 80%;">
+                                <div class="card-body px-4 py-4-5">
+                                    <div class="row">
+                                        <div class="px-1 col-md-2 col-lg-12 col-xl-12 col-xxl-2 d-flex justify-content-start ">
+                                            <div class="stats-icon red mb-2">
+                                                <i class="fa-solid fa-bed-pulse"></i>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="text-muted font-semibold">Followers</h6>
-                                        <h6 class="font-extrabold mb-0">183.000</h6>
+                                        <div class="col-md-10 col-lg-12 col-xl-12 col-xxl-10">
+                                            <h6 class="text-muted font-semibold">Lihat Pasien</h6>
+                                            <h6 class="font-extrabold mb-0">Melihat data pasien secara lengkap</h6>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
-                    <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card">
-                            <div class="card-body px-4 py-4-5">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                                        <div class="stats-icon green mb-2">
-                                            <i class="iconly-boldAdd-User"></i>
+
+                    <div class="col-6 col-lg-4 col-md-6">
+                        <a href="<?= base_url('PatientControlScheduleManagement') ?>">
+                            <div class="card" style="height: 80%;">
+                                <div class="card-body px-4 py-4-5">
+                                    <div class="row">
+                                        <div class="px-1 col-md-2 col-lg-12 col-xl-12 col-xxl-2 d-flex justify-content-start ">
+                                            <div class="stats-icon blue mb-2">
+                                                <i class="fa-solid fa-hospital-user"></i>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="text-muted font-semibold">Following</h6>
-                                        <h6 class="font-extrabold mb-0">80.000</h6>
+                                        <div class="col-md-10 col-lg-12 col-xl-12 col-xxl-10">
+                                            <h6 class="text-muted font-semibold">Manajemen Jadwal Kontrol Pasien</h6>
+                                            <h6 class="font-extrabold mb-0">Mengatur jadwal kontrol pasien</h6>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
-                    <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card">
-                            <div class="card-body px-4 py-4-5">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                                        <div class="stats-icon red mb-2">
-                                            <i class="iconly-boldBookmark"></i>
+                    <div class="col-6 col-lg-4 col-md-6">
+                        <a href="<?= base_url('PatientManagement') ?>">
+                            <div class="card" style="height: 80%;">
+                                <div class="card-body px-4 py-4-5">
+                                    <div class="row">
+                                        <div class="px-1 col-md-2 col-lg-12 col-xl-12 col-xxl-2 d-flex justify-content-start ">
+                                            <div class="stats-icon green mb-2">
+                                                <i class="fa-solid fa-person"></i>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="text-muted font-semibold">Saved Post</h6>
-                                        <h6 class="font-extrabold mb-0">112</h6>
+                                        <div class="col-md-10 col-lg-12 col-xl-12 col-xxl-10">
+                                            <h6 class="text-muted font-semibold">Manajemen Pasien</h6>
+                                            <h6 class="font-extrabold mb-0">Melihat detail data diri pasien</h6>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
+
                 </div>
             </div>
 
