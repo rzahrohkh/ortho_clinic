128517afa2e593fbf5c47e5563303bb0169536d6
rzahrohkh <170441100011@student.trunojoyo.ac.id>
2023-01-07 21:02:01 +0700

    fixing patient fiture (dashboard patient)

--- application/controllers/Patient.php
+++ application/controllers/Patient.php
@@ -20,9 +20,11 @@ class Patient extends CI_Controller
 		$data['jumlahNotifScheduleControl'] = $this->ControlPatient_model->get_jumlah_notif($season_patient);
 		$data['scheduleControlPatient'] = $this->ControlPatient_model->get_jadwal_control_by_id_patient($season_patient);
 		$data['user'] = $this->User_model->get_patient_and_role_byID($season_patient);
-		$hariControl = date("D", strtotime($data['scheduleControlPatient']['date_control_patient']));
-		$data['dayControlPatient'] = hari_ini($hariControl);
-		$data['dateControlPatient'] = tgl_indo($data['scheduleControlPatient']['date_control_patient']);
+		if($data['scheduleControlPatient']){
+			$hariControl = date("D", strtotime($data['scheduleControlPatient']['date_control_patient']));
+			$data['dayControlPatient'] = hari_ini($hariControl);
+			$data['dateControlPatient'] = tgl_indo($data['scheduleControlPatient']['date_control_patient']);
+		}
 		$this->load->view('templates_dp/patient/header', $data);
 		$this->load->view('templates_dp/worker/sidebar', $data);
 		$this->load->view('dp/patient/index', $data);
