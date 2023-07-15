07b3ce3918d84fc3b419059f3c2fbfff234e1e22
rzahrohkh <170441100011@student.trunojoyo.ac.id>
2023-07-15 10:04:41 +0700

    adding underscore code

--- application/libraries/Pdf.php
+++ application/libraries/Pdf.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-require_once(dirname(_FILE_) . '/dompdf/autoload.inc.php');
+require_once(dirname(__FILE__) . '/dompdf/autoload.inc.php');
 
 class Pdf
 {
